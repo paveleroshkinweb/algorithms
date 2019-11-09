3fe7e745fc64545e2fe7964decce06b879b1d292
paveleroshkinweb <eroshkin321@gmail.com>
2019-11-09 19:20:13 +0300

    add max_chain_length

--- python-algorithms/src/labs/Lab4(HashMap)/src/main.py
+++ python-algorithms/src/labs/Lab4(HashMap)/src/main.py
@@ -2,18 +2,22 @@ from hash_map_chains import HashMapChains
 import random
 import hash
 import math
+from functools import reduce
 
-size = 50007
+size = 30007
 random_range = 50000
+keys_size = 37000
 value = 5
 
 
 def get_metrics(hash_fn):
     map = HashMapChains(size, hash_fn)
-    for i in range(size):
+    for i in range(keys_size):
         random_key = random.randint(1, random_range)
         map.put(random_key, value)
-    return {'average-chain-length': get_average_chain_length(map), 'medium-chain-length': get_medium_chain_length(map)}
+    return {'average-chain-length': get_average_chain_length(map),
+            'medium-chain-length': get_medium_chain_length(map),
+            'max-chain-length': get_max_chain_length(map)}
 
 
 def get_average_chain_length(map):
@@ -28,9 +32,13 @@ def get_average_chain_length(map):
 
 
 def get_medium_chain_length(map):
-    buckets = map.buckets;
+    buckets = list(filter(lambda bucket: len(bucket) > 0, map.buckets))
     buckets.sort(key=lambda bucket: len(bucket))
     return len(buckets[len(buckets) // 2])
+
+
+def get_max_chain_length(map):
+    return reduce(lambda max_chain, bucket: max(max_chain, len(bucket)), map.buckets, 0)
 
 
 if __name__ == '__main__':
