87f073274e6445281782132f4685be54e195fd05
paveleroshkinweb <eroshkin321@gmail.com>
2019-12-13 23:07:51 +0300

    refactor robot problem

--- python-algorithms/src/labs/Lab6(Huffman Algorithm)/src/robot_problem.py
+++ python-algorithms/src/labs/Lab6(Huffman Algorithm)/src/robot_problem.py
@@ -10,20 +10,7 @@ def create_matrix(n):
 
 
 def fill_matrix(matrix):
-    fill_init_elements(matrix)
-    count_ways_for_each_cell(matrix)
-
-
-def fill_init_elements(matrix):
-    length = len(matrix)
-    matrix[length - 1][0] = 0
-    for i in range(0, length):
-        matrix[i][0] = 1
-        matrix[length - 1][i] = 1
-
-
-def count_ways_for_each_cell(matrix):
     length = len(matrix)
     for i in range(1, length):
         for j in range(length - 2, -1, -1):
-            matrix[j][i] = matrix[j+1][i] + matrix[j][i-1]
+            matrix[j][i] = (matrix[j + 1][i] or 1) + (matrix[j][i - 1] or 1)
