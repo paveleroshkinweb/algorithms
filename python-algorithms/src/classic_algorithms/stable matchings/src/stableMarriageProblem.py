14cd0e3ddb45968beb377736fe0f238b909a92d9
paveleroshkinweb <eroshkin321@gmail.com>
2020-04-21 17:51:56 +0300

    update stableMarriageProblem

--- python-algorithms/src/classic_algorithms/stable matchings/src/stableMarriageProblem.py
+++ python-algorithms/src/classic_algorithms/stable matchings/src/stableMarriageProblem.py
@@ -1,33 +1,18 @@
-def stable_marriage_problem(man_preferences, girl_preferences):
-    length = len(man_preferences)
-    mans_indexes = [0 for _ in range(length)]
-    pairs = dict([(i, None) for i in range(length)])
-    available_mans = set([i for i in range(length)])
-    engaged_girls = set()
-    while len(available_mans) > 0:
-        available_man = next(iter(available_mans))
-        girl = man_preferences[available_man][mans_indexes[available_man]]
-        if girl not in engaged_girls:
-            pairs[available_man] = girl
-            engaged_girls.add(girl)
-            available_mans.remove(available_man)
+def stable_marriage_problem(man_p, girl_p):
+    mans_indexes = [0 for _ in range(len(man_p))]
+    pairs = {}
+    available_mans = list(range(len(man_p)))
+    while len(available_mans) != 0:
+        available_man = available_mans[-1]
+        preferred_girl = man_p[available_man][mans_indexes[available_man]]
+        if preferred_girl not in pairs:
+            available_mans.pop()
+            pairs[preferred_girl] = available_man
         else:
-            current_man = find_man(pairs, girl)
-            girl_current_man_preference = girl_preferences[girl].index(current_man)
-            girl_available_man_preference = girl_preferences[girl].index(available_man)
-            if girl_current_man_preference > girl_available_man_preference:
-                pairs[available_man] = girl
-                available_mans.add(current_man)
-                available_mans.remove(available_man)
-                mans_indexes[current_man] += 1
-            else:
-                mans_indexes[available_man] += 1
+            current_man = pairs[preferred_girl]
+            if girl_p[preferred_girl].index(current_man) > girl_p[preferred_girl].index(available_man):
+                pairs[preferred_girl] = available_man
+                available_mans.pop()
+                available_mans.append(current_man)
+        mans_indexes[available_man] += 1
     return pairs
-
-
-def find_man(pairs: dict, girl):
-    items = pairs.items()
-    for item in items:
-        if item[1] == girl:
-            return item[0]
-    return None
--- python-algorithms/src/classic_algorithms/stable matchings/test/test_stable_match.py
+++ python-algorithms/src/classic_algorithms/stable matchings/test/test_stable_match.py
@@ -1,5 +1,6 @@
 import unittest
 from StableMatch import StableMatch
+from stableMarriageProblem import stable_marriage_problem
 
 
 class TestStableMatch(unittest.TestCase):
@@ -22,5 +23,19 @@ class TestStableMatch(unittest.TestCase):
         pairs_by_preferences = stable_match.get_stable_pairs_by_preferences()
         self.assertEqual({0: 4, 1: 0, 2: 3, 3: 1, 4: 2}, pairs_by_estimations)
         self.assertEqual(46, stable_match.total_efficiency(pairs_by_estimations))
-        self.assertEqual(51, stable_match.total_efficiency(pairs_by_preferences));
+        self.assertEqual(51, stable_match.total_efficiency(pairs_by_preferences))
 
+    def test_stable_marriage_problem(self):
+        man_p = [
+            [1, 0, 2, 3],
+            [2, 1, 0, 3],
+            [3, 2, 1, 0],
+            [1, 0, 3, 2],
+        ]
+        girl_p = [
+            [2, 3, 0, 1],
+            [0, 2, 3, 1],
+            [3, 2, 1, 0],
+            [0, 2, 1, 3],
+        ]
+        self.assertDictEqual({1: 0, 3: 2, 2: 1, 0: 3}, stable_marriage_problem(man_p, girl_p))
