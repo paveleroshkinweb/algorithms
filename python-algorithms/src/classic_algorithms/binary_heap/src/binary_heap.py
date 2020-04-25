ffc693d3f9639091938e2779677557b03ab8fc45
paveleroshkinweb <eroshkin321@gmail.com>
2020-04-25 20:26:16 +0300

    refactor binary_heap

--- python-algorithms/src/classic_algorithms/binary_heap/src/binary_heap.py
+++ python-algorithms/src/classic_algorithms/binary_heap/src/binary_heap.py
@@ -1,61 +1,68 @@
-import math
+class BinaryHeap:
 
-class MaxBinaryHeap:
-
-    def __init__(self):
+    def __init__(self, compare, elements=[]):
+        self.compare = compare
         self.values = []
+        self.multiple_insert(elements)
 
-    def insert(self, value):
-        self.values.append(value)
-        self.bubble_up()
+    def insert(self, element):
+        self.values.append(element)
+        self._bubble_up()
 
-    def bubble_up(self):
-        value = self.values[-1]
-        parent_index = (len(self.values) - 1) // 2
-        new_element_index = len(self.values) - 1
-        while self.values[parent_index] < value and new_element_index > 0:
-            self.swap(parent_index, new_element_index)
-            new_element_index = parent_index
-            parent_index = (new_element_index - 1) // 2
+    def multiple_insert(self, elements):
+        for element in elements:
+            self.insert(element)
 
-    def show_max(self):
-        return self.values[0] if len(self.values) > 0 else None
-
-    def extract_max(self):
+    def extract(self):
         if len(self.values) == 0:
             return None
-        max_value = self.values[0]
-        self.swap(0, -1)
-        del self.values[-1]
-        if len(self.values) > 0:
-            self.bubble_down()
-        return max_value
+        element = self.values[0]
+        self._swap(0, -1)
+        self.values.pop(-1)
+        if len(self.values) > 1:
+            self._bubble_down()
+        return element
 
-    def bubble_down(self):
+    def _bubble_up(self):
+        parent_index = (len(self.values) - 1) // 2
+        element_index = len(self.values) - 1
+        while self.compare(self.values[parent_index], self.values[element_index]) and element_index > 0:
+            self._swap(parent_index, element_index)
+            element_index = parent_index
+            parent_index = (parent_index - 1) // 2
+
+    def _bubble_down(self):
         index = 0
-        while True:
-            value = self.values[index]
-            indexes = {'left_child_index': 2 * index + 1, 'right_child_index': 2 * index + 2}
-            elements_indexes_map = {}
-            for index_name in indexes.keys():
-                index_number = indexes[index_name]
-                if self.is_valid_index(index_number):
-                    elements_indexes_map[self.values[index_number]] = index_number
-            if len(elements_indexes_map) == 0:
-                return
-            max_child = max(elements_indexes_map.keys())
-            if max_child > value:
-                max_child_index = elements_indexes_map[max_child]
-                self.swap(index, max_child_index)
-                index = max_child_index
-            else:
-                return
+        child_to_swap = self._get_child_to_swap(index)
+        while child_to_swap != index:
+            self._swap(index, child_to_swap)
+            index, child_to_swap = child_to_swap, self._get_child_to_swap(child_to_swap)
 
-    def is_valid_index(self, index):
+    def _swap(self, i, j):
+        self.values[i], self.values[j] = self.values[j], self.values[i]
+
+    def _left_child_index(self, parent_index):
+        return 2 * parent_index + 1
+
+    def _right_child_index(self, parent_index):
+        return 2 * parent_index + 2
+
+    def _children_indexes(self, parent_index):
+        return [self._left_child_index(parent_index), self._right_child_index(parent_index)]
+
+    def _is_valid_index(self, index):
         return 0 <= index < len(self.values)
 
-    def swap(self, i, j):
-        [self.values[i], self.values[j]] = [self.values[j], self.values[i]]
+    def _get_child_to_swap(self, parent_index):
+        left_child = self._left_child_index(parent_index)
+        right_child = self._right_child_index(parent_index)
+        indexes = [parent_index, left_child, right_child]
+        valid_indexes = [index for index in indexes if self._is_valid_index(index)]
+        return self._get_max_element_index(valid_indexes)
 
-    def size(self):
-        return len(self.values)
+    def _get_max_element_index(self, indexes):
+        max_element_index = indexes[0]
+        for index in indexes:
+            if self.compare(self.values[max_element_index], self.values[index]):
+                max_element_index = index
+        return max_element_index
--- python-algorithms/src/classic_algorithms/binary_heap/test/test_binary_heap.py
+++ python-algorithms/src/classic_algorithms/binary_heap/test/test_binary_heap.py
@@ -1,12 +1,12 @@
 import unittest
 
-from binary_heap import MaxBinaryHeap
+from binary_heap import BinaryHeap
 
 
-class TestMaxBinaryHeap(unittest.TestCase):
+class TestBinaryHeap(unittest.TestCase):
 
     def test_insert(self):
-        bh = MaxBinaryHeap()
+        bh = BinaryHeap(compare=lambda a, b: a < b)
         bh.insert(3)
         bh.insert(3)
         bh.insert(2)
@@ -14,8 +14,8 @@ class TestMaxBinaryHeap(unittest.TestCase):
         bh.insert(4)
         self.assertSequenceEqual([4, 3, 2, 3], bh.values)
 
-    def test_extract_max(self):
-        bh = MaxBinaryHeap()
+    def test_extract(self):
+        bh = BinaryHeap(compare=lambda a, b: a < b)
         bh.insert(4)
         bh.insert(1)
         bh.insert(1)
@@ -23,13 +23,13 @@ class TestMaxBinaryHeap(unittest.TestCase):
         bh.insert(3)
         bh.insert(2)
         bh.insert(2)
-        self.assertEqual(4, bh.extract_max())
-        self.assertEqual(3, bh.extract_max())
-        self.assertEqual(3, bh.extract_max())
-        self.assertEqual(2, bh.extract_max())
-        self.assertEqual(2, bh.extract_max())
-        self.assertEqual(1, bh.extract_max())
-        self.assertEqual(1, bh.extract_max())
+        self.assertEqual(4, bh.extract())
+        self.assertEqual(3, bh.extract())
+        self.assertEqual(3, bh.extract())
+        self.assertEqual(2, bh.extract())
+        self.assertEqual(2, bh.extract())
+        self.assertEqual(1, bh.extract())
+        self.assertEqual(1, bh.extract())
 
 
 if __name__ == 'main':
