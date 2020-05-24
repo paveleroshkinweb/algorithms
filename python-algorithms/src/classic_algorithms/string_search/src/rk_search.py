2d0eb3096054015506d9539cd1049e9913304e69
paveleroshkinweb <eroshkin321@gmail.com>
2020-05-24 18:15:07 +0300

    update rk_search

--- python-algorithms/src/classic_algorithms/string_search/src/rk_search.py
+++ python-algorithms/src/classic_algorithms/string_search/src/rk_search.py
@@ -1,29 +1,23 @@
 def rk_search(string, substring):
     assert len(string) >= len(substring)
-    r = 256
-    q = 15487469
-    substring_hash = hash(substring, len(substring), r, q)
-    text_hash = hash(string, len(substring), r, q)
-    rm = get_rm(len(substring), r, q)
-    if substring_hash == text_hash:
+    base = 31
+    prime = 101
+    str_len, sub_len = len(string), len(substring)
+    string_hash, substring_hash = hashes(string, substring, base, prime)
+    rm = pow(base, sub_len - 1) % prime
+    if substring_hash == string_hash and substring == string[0:sub_len]:
         return 0
-    for i in range(len(substring), len(string)):
-        text_hash = (text_hash + q - rm * ord(string[i-len(substring)]) % q) % q
-        text_hash = (text_hash * r + ord(string[i])) % q
-        if text_hash == substring_hash and substring == string[i - len(substring) + 1: i + 1]:
-            return i - len(substring) + 1
+    for i in range(1, str_len - sub_len + 1):
+        string_hash = (string_hash + prime - rm * ord(string[i - 1]) % prime) % prime
+        string_hash = (string_hash * base + ord(string[i+sub_len-1])) % prime
+        if string_hash == substring_hash and substring == string[i:i+sub_len]:
+            return i
     return -1
 
 
-def get_rm(length, r, q):
-    rm = 1
-    for i in range(1, length):
-        rm = (r * rm) % q
-    return rm
-
-
-def hash(string, m, r, q):
-    h = 0
-    for i in range(m):
-        h = (r * h + ord(string[i])) % q
-    return h
+def hashes(string, substring, base, prime):
+    string_hash = substring_hash = 0
+    for i in range(len(substring)):
+        string_hash = (base * string_hash + ord(string[i])) % prime
+        substring_hash = (base * substring_hash + ord(substring[i])) % prime
+    return string_hash, substring_hash
