a92b83bcefa3478afc2e525206cad28e31498719
paveleroshkinweb <eroshkin321@gmail.com>
2020-05-30 15:12:13 +0300

    add union_find data structure

---
+++ python-algorithms/src/classic_algorithms/union_find/src/union_find.py
@@ -0,0 +1,23 @@
+class UnionFind:
+
+    def __init__(self, elements):
+        self.parents = {key: key for key in elements}
+
+    def find(self, element):
+        if element not in self.parents:
+            return None
+        path = []
+        while self.parents[element] != element:
+            path.append(element)
+            element = self.parents[element]
+        for vertex in path:
+            self.parents[vertex] = element
+        return element
+
+    def is_same_set(self, element1, element2):
+        return self.find(element1) == self.find(element2)
+
+    def union(self, element1, element2):
+        parent1, parent2 = self.parents[element1], self.parents[element2]
+        self.parents[parent2] = parent1
+        return parent1
