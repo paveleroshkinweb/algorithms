1a2f3d0e2a19bc23322aab1cba4b1f009e1e3737
paveleroshkinweb <eroshkin321@gmail.com>
2020-05-30 15:14:19 +0300

    update union method

--- python-algorithms/src/classic_algorithms/union_find/src/union_find.py
+++ python-algorithms/src/classic_algorithms/union_find/src/union_find.py
@@ -18,6 +18,8 @@ class UnionFind:
         return self.find(element1) == self.find(element2)
 
     def union(self, element1, element2):
+        if element1 not in self.parents or element2 not in self.parents:
+            return None
         parent1, parent2 = self.parents[element1], self.parents[element2]
         self.parents[parent2] = parent1
         return parent1
