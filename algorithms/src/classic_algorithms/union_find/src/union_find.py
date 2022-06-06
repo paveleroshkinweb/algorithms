d1013fe1f97a75d28e498e10fa6ced0cdb6b14e2
paveleroshkinweb <eroshkin321@gmail.com>
2022-06-06 18:56:42 +0300

    add new algorithms

--- algorithms/src/classic_algorithms/union_find/src/union_find.py
+++ algorithms/src/classic_algorithms/union_find/src/union_find.py
@@ -1,17 +1,29 @@
-class UnionFind:
+class UnionFind1:
 
     def __init__(self, elements):
         self.parents = {key: key for key in elements}
 
     def find(self, element):
-        if element not in self.parents:
-            return None
-        path = []
+        return self.parents[element]
+
+    def is_same_set(self, element1, element2):
+        return self.parents[element1] == self.parents[element2]
+
+    def union(self, element1, element2):
+        for key in self.parents:
+            if self.parents[key] == element2:
+                self.parents[key] = element1
+        return element1
+
+
+class UnionFind2:
+
+    def __init__(self, elements):
+        self.parents = {key: key for key in elements}
+
+    def find(self, element):
         while self.parents[element] != element:
-            path.append(element)
             element = self.parents[element]
-        for vertex in path:
-            self.parents[vertex] = element
         return element
 
     def is_same_set(self, element1, element2):
@@ -20,6 +32,12 @@ class UnionFind:
     def union(self, element1, element2):
         if element1 not in self.parents or element2 not in self.parents:
             return None
-        parent1, parent2 = self.parents[element1], self.parents[element2]
+        parent1, parent2 = self.find(element1), self.find(element2)
         self.parents[parent2] = parent1
         return parent1
+
+
+class UnionFind3:
+
+    def __init__(self, elements):
+        self.parents = {key: (key, 1) for key in elements}
--- algorithms/src/math/bits/utils.py
+++ algorithms/src/math/bits/utils.py
@@ -2,5 +2,30 @@ def is_divisible_by_power_2(n, power):
     assert power >= 0
     if power == 0:
         return True
-    checker = 2 ** (power) - 1
+    checker = (1 << power) - 1
     return (n & checker) == 0
+
+
+def set_bit_1(n, position):
+    return n | 2 ** position
+
+
+def clear_bit_1(n, position):
+    return n & ~(2 ** (position))
+
+
+def read_bit_1(n, position):
+    return n & (2 ** position) >> position
+
+
+def set_bit_2(n, position):
+    return n | (1 << position)
+
+
+def clear_bit_2(n, position):
+    return n & ~(1 << position)
+
+
+def read_bit_2(n, position):
+    return n & (1 << position) >> position
+
