b7dc1950f859e0ae47c3f57455ab6eece6d1ca0d
paveleroshkinweb <eroshkin321@gmail.com>
2022-08-08 13:27:46 +0300

    add recursive pow

--- algorithms/src/math/power.py
+++ algorithms/src/math/power.py
@@ -7,3 +7,13 @@ def pow(n, power):
         power >>= 1
         n_degree *= n_degree
     return result
+
+
+def pow2(n, power):
+    if power == 0:
+        return 1
+    if power == 1:
+        return n
+    if power & 1 == 0:
+        return pow2(n, power // 2) ** 2
+    return pow2(n, power-1) * n
